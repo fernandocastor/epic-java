cdc2dadced074b1da9def22e6d66850d58ecd522
Thiago Silva <thiago@metareload.com>
2012-05-16 22:35:03 -0300

    Changing script input syntax and improving notification.
    -syntax was clashing with array parameters (the uses of []).
    
    -overriding errors was generating bad inputs to the script. Workarounds
     were added to make a saner script execution on such errors.

--- src/share/classes/com/sun/tools/javac/comp/ScriptPropagate.java
+++ src/share/classes/com/sun/tools/javac/comp/ScriptPropagate.java
@@ -16,24 +16,46 @@ import com.sun.tools.javac.tree.JCTree.JCMethodInvocation;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
  * @author thiago
  */
 public class ScriptPropagate {
+    public static List<String> lst = new ArrayList<String>();
+    public static List<String> hierarchy_first = new ArrayList<String>();
+    public static List<String> hierarchy_second = new ArrayList<String>();
+
     public static void logPropagateError(Symbol.MethodSymbol m, Symbol.MethodSymbol overrided, Type.ClassType ct) {
-        String s = "{!";
         Symbol.ClassSymbol mcs = (Symbol.ClassSymbol) m.owner;
         Symbol.ClassSymbol cs = (Symbol.ClassSymbol) overrided.owner;
-        s += mcs.fullname + "::" + m + "!} {!"+ cs.fullname + "::" + overrided + "!} should throw {!"+ct.tsym+"!}";
+
+        if (((Type.ClassType)mcs.type).supertype_field != cs.type) {
+            return;
+        }
+
+        String first = "{*" + mcs.fullname + "::" + m + "*}";
+        String second = "{*"+ cs.fullname + "::" + overrided + "*}";
+        String s = first + " " + second + " should throw {*"+ct.tsym+"*}";
+
+        for (String f : hierarchy_first) {
+            if (f.equals(first)) return;
+        }
+        for (String sec : hierarchy_second) {
+            if (sec.equals(second)) return;
+        }
+
+        hierarchy_first.add(first);
+        hierarchy_second.add(second);
         execute(s);
     }
 
     public static void logPropagateError(PendingExit exit, JCMethodDecl tree) {
-        String s = "[!"+tree.sym.owner.toString()+"::" + tree.sym.toString()
-                + "!] should throw [!" + exit.thrown.toString()
-                + "!] because it calls [!";
+        String s = "[*"+tree.sym.owner.toString()+"::" + tree.sym.toString()
+                + "*] should throw [*" + exit.thrown.toString()
+                + "*] because it calls [*";
         //+ " because of " + f.selected.type.toString() + "::"+f.name + "["+f.type+"]");
         //+ " because of " + f.selected.type.toString() + "::" +f.sym;
         if (exit.tree.getTag() == JCTree.APPLY) {
@@ -53,14 +75,23 @@ public class ScriptPropagate {
                 default:
                     System.err.println("++BUG!!");
             }
-            s += "!]";
+            s += "*]";
         } else {
-            s += "ORIGIN!]";
+            s += "ORIGIN*]";
         }
         execute(s);
     }
 
     static private void execute(String s) {
+
+
+        for (String old : lst) {
+            if(old.equals(s)) return;
+        }
+        lst.add(s);
+
+        System.out.println(s);
+        if (true) return;
         try {
             String[] cmd = {"pjavac-script", s};
 
