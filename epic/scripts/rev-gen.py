5464558c6bb5513b5188809f70bd1ca3bbf09506
Thiago Silva <thiago@metareload.com>
2013-02-19 23:50:55 -0300

    Some progress on the re-coding of the reverse engineering scripts

--- epic/scripts/rev-gen.py
+++ epic/scripts/rev-gen.py
@@ -8,16 +8,121 @@ dbname = sys.argv[1]
 
 con = pymongo.Connection("localhost")
 db = con[dbname]
-col = db.nodes
 
 
-def print_propagate(node, path):
-    callers = col.find({"calls":node["_id"], "ex":node["ex"]})
-    if callers.count() == 0:
-        print "propagating " + node["ex"] + ": " + path + ";"
+output = ""
+def append(string):
+    global output
+    output = output + string + "\n"
+
+def prepend(string):
+    global output
+    output = string + "\n" + output
+
+
+
+def to_sig(term):
+    if len(term["subs"]) == 0:
+        return term["type"]+"."+term["name"]
     else:
-        for caller in callers:
-            print_propagate(caller, path + " => " + caller["owner"] + "." + caller["name"])
+        terms = db.terms
+        sub_terms = [terms.find_one({"_id":sub_id}) for sub_id in term["subs"]]
+        return "{"+', '.join([t["type"] for t in sub_terms]) + " <: " + term["type"] + "}." + term["name"]
 
-for root in col.find({"calls":None}):
-    print_propagate(root, root["owner"] + "." + root["name"])
+def print_path(path):
+    terms = db.terms
+    ts = [terms.find_one({"_id":tid}) for tid in path["terms"]]
+    append("propagating " + path["ex"] + ": " + ' => '.join([to_sig(t) for t in ts]) + ";")
+
+def print_poly(poly):
+    prepend("propagating " + poly["ex"] + ": " + to_sig(poly) + ";")
+
+def doit():
+    # ordinary paths
+    paths = db.paths
+    for path in paths.find():
+        print_path(path)
+
+    terms = db.terms
+    paths = db.paths
+    # poly terms with no path
+    for poly in terms.find({"subs":{"$ne":[]}}):
+        if paths.find_one({"terms":poly["_id"]}) == None:
+            print_poly(poly)
+
+    print output
+
+doit()
+
+# col = db.nodes
+
+# output = ""
+# def append(string):
+#     global output
+#     output = output + string + "\n"
+
+# def prepend(string):
+#     global output
+#     output = string + "\n" + output
+
+# # def is_poly(node):
+# #     for poly in poly_nodes:
+# #         if poly["super"] == node["type"] and poly["name"] == node["name"]:
+# #             return True
+# #     return False
+
+# def sig_for(node):
+#     if node["parent"] == None:
+#         return node["type"] + "." + node["name"]
+#     else:
+#         parent = col.find_one({"_id":node["parent"], "ex":node["ex"]})
+#         children = [x for x in col.find({"parent":parent["_id"], "ex":node["ex"]})]
+#         for poly in poly_nodes:
+#             if poly["ex"] == parent["ex"] and poly["super_type"] == parent["type"]:
+#                 poly["done"] = True
+#         return "{" + ", ".join([c["type"] for c in children]) + "<:" + parent["type"] + "}." + node["name"]
+
+# def poly_covering(node):
+#     for poly in poly_nodes:
+#         if poly["ex"] == node["ex"] and\
+#                 poly["name"] == node["name"] and\
+#                 (node["type"] == poly["super_type"] or\
+#                      node["type"] in poly["subs"]):
+#             return poly
+#     return None
+
+# def print_propagate(ex, path):
+#     poly = poly_covering(path[0])
+#     if len(path) == 1 and poly != None:
+#         poly["done"] = True
+#         prepend("propagating " + poly["ex"] + ": " + "{" + ', '.join(poly["subs"]) + " <: " + poly["super_type"] + "}." + poly["name"] + ";")
+#     else:
+#         append("propagating " + ex + ": " + ' => '.join([sig_for(x) for x in path]) + ";")
+
+# def build_paths(node, path):
+#     callers = col.find({"calls":node["_id"], "ex":node["ex"]})
+#     if callers.count() == 0:
+#         if node["parent"]:
+#             parent = col.find_one({"_id":node["parent"]})
+#             build_paths(parent, path)
+#         else:
+#             print_propagate(node['ex'], path)
+
+#     else:
+#         for caller in callers:
+#             build_paths(caller, path + [caller])
+
+#         if node["parent"]:
+#             parent = col.find_one({"_id":node["parent"]})
+#             build_paths(parent, path + [parent])
+
+# for root in col.find({"calls":None}):
+#     build_paths(root, [root])
+
+
+
+# for poly in poly_nodes:
+#     if "done" not in poly:
+#         prepend("propagating " + poly["ex"] + ": " + "{" + ', '.join(poly["subs"]) + " <: " + poly["super_type"] + "}." + poly["name"] + ";")
+
+# print output
--- epic/scripts/rev-load.py
+++ epic/scripts/rev-load.py
@@ -1,21 +1,40 @@
 #!/usr/bin/python
 
-#
-#
-#[!testando.A::f(java.lang.Object)!] should throw [!V!] because it calls [!testando.A::g()!]
+# DB structure:
+#   db.terms = {signature,name,type,ex, subs:[...]} //subs is array of term ids
+#   db.paths = {ex,terms:[...]} //terms is array of term ids
 
-# 0: {
-#    fullname: "testando.A::f(X)"
-#    name: "f",
-#    owner: "testando.A",
-#    ex: [Exception, IOException]
-#    calls: "ORIGIN" | [3,4,5]
-# }
+# -each path corresponds to a single propagate statement
+#   -therefore, there may be terms with same signature differing on ex
+#   - ...     , there may be paths with same terms differing on ex
+# -Calling errors have the caller appended to the corresponding path's terms
+#  where callee is part of
+# -inheritance error adds subtype info to the term, based on ex
+#  (tied to ex, so we don't specify a tree of classes for every exception,
+#   since not all subtypes may throw all exceptions of the super type)
+
+# terms with subype info that are not part of a path should
+# have a propagate for them only.
+
+# examples of input:
+
+# [!A::f()!] should throw [!E!] because it calls [!ORIGIN!]
+#  [!A::g()!] should throw [!E!] because it calls [!A::f()!]
+#   [!A::h()!] should throw [!E!] because it calls [!A::g()!]
+#   [!A::i()!] should throw [!E!] because it calls [!A::g()!]
+#  [!A::j()!] should throw [!E!] because it calls [!A::f()!]
+
+# {!A::f()!} {!SuperA::f()!} should throw {!E!}
+
+#  [!B::k()!] should throw [!E!] because it calls [!SuperA::f()!]
 
 import sys
 import os
 import pymongo
 import re
+
+def dd(x):
+    print x
 
 input = sys.argv[1]
 dbname = os.environ["EPIC_DB"]
@@ -24,55 +43,98 @@ con = pymongo.Connection("localhost")
 db = con[dbname]
 
 def add_call_chain():
-    fullname, ex, origin = re.findall("\[!([^!]+)!\]", input)
-    owner, fname = fullname.split("::")
+    sig, ex, origin_sig = re.findall("\[!([^!]+)!\]", input)
+    ftype, fname = sig.split("::")
 
-    if fullname.startswith("<ano"):
+    # check for anonymous class
+    if sig.startswith("<ano"):
+        dd("its an anonymous!")
         col = db.err
         col.insert({"reason": "anonymous"})
         sys.exit(1)
 
-        col = db.nodes
+    terms = db.terms
 
     # don't add duplicate
-    n = col.find_one({"fullname":fullname, "ex":ex})
+    n = terms.find_one({"signature":sig,"ex":ex})
     if n != None:
-        raise Exception("there shouldn't be a node here")
+        raise Exception("there shouldn't be a term here")
 
-    # check for anonymous class
-    if origin == "ORIGIN":
-        col.insert({"fullname":fullname,
-                    "name": fname,
-                    "owner": owner,
-                    "ex":ex,
-                    "calls": None})
+    # get a term for the caller
+    term = terms.find_one({"signature":sig, "ex":ex})
+    if term != None:
+        dd("A term was found for " + sig)
+        term_id = term["_id"]
     else:
-        callee = col.find_one({"fullname":origin, "ex":ex})
-        if callee == None:
-            raise Exception("where is the callee?")
-        col.insert({"fullname": fullname,
-                    "name": fname,
-                    "owner": owner,
-                    "ex": ex,
-                    "calls": callee["_id"]})
+        dd("Creating a term for " + sig)
+        term_id = terms.insert({"name": fname,
+                                "type": ftype,
+                                "signature":sig,
+                                "ex": ex,
+                                "subs":[]})
+
+    # if ORIGIN, create a new path with term as head
+    # else, find the paths with (origin,ex) and append term to it
+    #   if (origin) is not tail, duplicate the path, prune rest, and append term
+    paths = db.paths
+    if origin_sig == "ORIGIN":
+        dd("its origin, creating a path...")
+        paths.insert({"ex":ex,
+                      "terms":[term_id]})
+    else:
+        dd("its not origin, finding paths...")
+        org = terms.find_one({"signature": origin_sig, "ex":ex})
+        if org == None:
+            raise Exception("we *should* have an origin here")
+        p = paths.find_one({"ex":ex,"terms":org["_id"]})
+
+        if p == None: # super::f may not be part of any path, because of overriding error
+                      # (see add_hierarchy below). Lets create one with two slots
+            paths.insert({"ex":ex,
+                          "terms":[org["_id"], term_id]})
+        else:
+            aterms = p["terms"]
+            spliced = aterms[:aterms.index(org["_id"])] + [org["_id"]]
+            if spliced == aterms: # org is tail, just append and update
+                dd("a path has org as tail, updating")
+                p["terms"].append(term_id)
+                paths.update({"_id":p["_id"]}, {"$set":{"terms":p["terms"]}})
+            else: # org was in the middle of the path, create another path
+                dd("a path has org in middle, duplicating path")
+                spliced.append(term_id)
+                paths.insert({"ex":ex,
+                              "terms":spliced})
 
 def add_hierarchy():
-    subclass, superclass, ex = re.findall("{!([^!]+)!}", input)
+    subsig, supersig, ex = re.findall("{!([^!]+)!}", input)
 
-    col = db.poly
+    terms = db.terms
 
-    super_type, super_fname = superclass.split("::")
-    super_node = col.find_one({"name":super_fname, "type":super_type, "ex":ex})
-    if super_node == None:
-        super_node_id = col.insert({"name":super_fname, "type":super_type, "parent":None, "ex":ex})
+    sub_type, fname = subsig.split("::")
+    super_type, fname = supersig.split("::")
 
-    sub_type, sub_fname = subclass.split("::")
-    col.insert({"name":sub_fname, "type": sub_type, "parent": super_node_id, "ex":ex})
+    ts = terms.find({"signature":supersig,"ex":ex})
+    if ts.count() > 0: # just add sub info and we are done
+        for t in ts:
+            term["subs"].append(sub_type)
+            terms.update({"_id":term["_id"]}, {"$set":{"subs":term["subs"]}})
+    else: #there is no path with base::f.
+          #create term for them. They are not part of a path, so we will have single propagates for them
+        sub = terms.find_one({"ex":ex, "signature":subsig})
+        if sub == None:
+            raise Exception("there should be a sub method here")
+
+        term_id = terms.insert({"name": fname,
+                                "type": super_type,
+                                "signature":supersig,
+                                "ex": ex,
+                                "subs":[sub["_id"]]})
 
 
 ####
 
 if input.startswith("[!"):
+    dd("adding call chain")
     add_call_chain()
 elif input.startswith("{!"):
     add_hierarchy()
